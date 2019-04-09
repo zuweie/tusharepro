150c77cb1945e47d081551f7a709d1d7bbcfdad6
zuweie <jojoe.wei@gmail.com>
2019-04-09 14:16:59 +0800

    添加一些有用的接口

--- test_tusharepro.js
+++ test_tusharepro.js
@@ -3,5 +3,27 @@
 const TusharePro = require('./tspro');
 
 const tushare = new TusharePro('93ecc4a57a1b58051591cf77c7e960c66af95ce980628fa518130444');
-
-tushare.pro_api('daily', {ts_code: '000001.SZ', start_date:'20180701', end_date:'20180718'}, 'open,high,low,close,trade_date');
+/*
+tushare.pro_api('daily', {ts_code: '000001.SZ', trade_date: '20190408'}, 'open,high,low,close,trade_date,ts_code')
+.then(data => {
+    console.log (JSON.stringify(data));
+}).catch( err => {
+    console.log (err);
+});
+*/
+/*
+tushare.stock_basic()
+.then(data => {
+    console.log (JSON.stringify(data));
+}).catch (err => {
+    console.log (err);
+});
+*/
+tushare.trade_cal()
+    .then(data => {
+        console.log (JSON.stringify(data));
+    })
+    .catch( err => {
+        console.log (err);
+    });
+//console.log (JSON.stringify(data));
--- tspro.js
+++ tspro.js
@@ -30,7 +30,7 @@ function pro_api (api_name, token, params, fields='') {
         const req = http.request(options, res => {
 
             if (res.statusCode != 200) {
-                reject (res.statusMessage);
+                reject ({'code':res.statusCode, 'errmsg':res.statusMessage});
             }
             
             let data = '';
@@ -40,12 +40,17 @@ function pro_api (api_name, token, params, fields='') {
                 data += chunk;
             }).on('end', () => {
                 //console.log(data);
-                reslove (data);
+                let stock_data = reformat_data(data);
+                if (stock_data.code === 0) {
+                    reslove (stock_data);
+                }else{
+                    reject (stock_data);
+                }
             });
             
 
         } ).on('error', err => {
-            reject (err.message);
+            reject ({'code':err.code, 'errmsg':err.message});
         });
 
         req.write(parameters);
@@ -54,6 +59,28 @@ function pro_api (api_name, token, params, fields='') {
     
 }
 
+ function reformat_data (rawdata) {
+     const raw_data = JSON.parse(rawdata);
+     let stock_data = {
+         code: raw_data.code,
+         errmsg: raw_data.msg ? raw_data.msg : 'ok',
+         data: null
+     };
+     if (raw_data.code == 0) {
+        stock_data.data = [];
+        const fields = raw_data.data.fields;
+        raw_data.data.items.forEach( item => {
+           let item_data = {};
+           for (let i=0; i<fields.length; ++i) {
+              item_data[fields[i]] = item[i];
+           }
+           stock_data.data.push(item_data);
+        });
+        stock_data.fields = fields;
+     }
+
+     return stock_data;
+ }
 
 function TusharePro (token) {
 
@@ -61,15 +88,34 @@ function TusharePro (token) {
 
 };
 
-TusharePro.prototype.pro_api = async function (api_name, params, fields=''){
-    try {
-        var data =  await pro_api(api_name, this.token, params, fields);
-    }catch(e) {
-        console.log(e);
-    }
-    
-    console.log(data);
+TusharePro.prototype.pro_api = function (api_name, params, fields=''){
+    return pro_api(api_name, this.token, params, fields);
+};
+
+TusharePro.prototype.stock_basic = function (is_hs='', list_status='L', exchange='', fields='') {
+    return pro_api('stock_basic', 
+        this.token,
+        {
+            is_hs:is_hs, 
+            list_status:list_status, 
+            exchange: exchange
+        },
+        fields);
+};
+
+TusharePro.prototype.trade_cal = function (exchange='', start_date='', end_date='', is_open='', fields='') {
+    return pro_api ('trade_cal', 
+        this.token,
+        {
+            exchange: exchange,
+            start_date: start_date,
+            end_date: end_date,
+            is_open: is_open,
+
+        },
+        fields);
 }
+
 
 
 module.exports = TusharePro;
